c1cc5de5b28e121df3dbd85f56912cbf53bb96f3
rubychilds <ruby.childs1@gmail.com>
2017-12-14 22:58:35 +0000

    converting getLaplacian1 into python

--- gen_laplacian/matting/getLaplacian1.py
+++ gen_laplacian/matting/getLaplacian1.py
@@ -9,39 +9,41 @@ def getLaplacian1(I, consts, epsilon=0.0000001, win_size=1):
   n = h
   m = w
   img_size = w * h
-  consts = cv2.erode(consts, ones(win_size*2+1))
+  consts = cv2.erode(consts, np.ones(win_size*2+1))
 
-  indsM = np.arange(1, img_size+1).reshape((h,w))
-  tlen = sum(sum(1-consts[win_size:-win_size-1, win_size:- win_size])) * (neb_size^2)
+  indsM = np.arange(1, img_size+1).reshape((w,h))
+  tlen = int(sum(sum(1-consts[win_size:-win_size, win_size:-win_size])) * (neb_size**2))
 
-  row_inds = np.zeros(tlen, 1)
-  col_inds = np.zeros(tlen, 1)
-  vals = np.zeros(tlen,1)
-  len = 0
-  for j=1+win_size:w-win_size:
-    for i=win_size+1:h-win_size:
-      if consts(i,j):
+  row_inds = np.zeros((tlen, 1))
+  col_inds = np.zeros((tlen, 1))
+  vals = np.zeros((tlen, 1))
+  length = 0
+  for j in range(win_size, w-win_size-1):
+    for i in range(win_size, h-win_size-1):
+      if consts[i,j]:
         continue
-      win_inds = indsM(i-win_size:i+win_size,j-win_size:j+win_size)
-      win_inds = win_inds(:)
-      winI = I(i-win_size:i+win_size,j-win_size:j+win_size,:)
-      winI = reshape(winI,neb_size,c)
-      win_mu = mean(winI,1)'
-      win_var = inv(winI*winI/neb_size-win_mu*win_mu +epsilon/neb_size*eye(c))
+      win_inds = indsM[i-win_size:i+win_size+1, j-win_size:j+win_size+1]
+      win_inds = win_inds.flatten()
+      winI = I[i-win_size:i+win_size+1, j-win_size:j+win_size+1,:]
+      winI = winI.reshape((neb_size, c))
+      win_mu = np.transpose(np.mean(winI, 0))
+      win_mu = win_mu.reshape((win_mu.shape[0], 1))
 
-      winI = winI-repmat(win_mu,neb_size,1)
-      tvals = (1+winI*win_var*winI)/neb_size
+      win_var = np.linalg.inv(np.matmul(np.transpose(winI/255.), winI/255.)/neb_size - win_mu/255.*np.transpose(win_mu/255.) + epsilon/neb_size*np.identity(c))
 
-      row_inds(1+len:neb_size^2+len)=reshape(repmat(win_inds,1,neb_size),
-                                             neb_size^2,1)
-      col_inds(1+len:neb_size^2+len)=reshape(repmat(win_inds,neb_size,1)
-                                             neb_size^2,1)
-      vals(1+len:neb_size^2+len)=tvals(:);
-      len = len+neb_size^2;
+      winI = winI/255. - np.tile(np.transpose(win_mu/255.), (neb_size, 1))
 
-  vals = vals(1:len)
-  row_inds=row_inds(1:len)
-  col_inds=col_inds(1:len)
+      tvals = (1 + np.matmul(np.matmul(winI, win_var),np.transpose(winI)))/neb_size
+
+      row_inds[length:neb_size**2 + length] = np.reshape(np.tile(win_inds.reshape(win_inds.shape[0], 1), (1, neb_size)), (neb_size**2, 1))
+      col_inds[length:neb_size**2 + length] = np.reshape(np.tile(np.transpose(win_inds.reshape(win_inds.shape[0], 1)), (neb_size, 1)), (neb_size**2, 1))
+      tvals = tvals.flatten()
+      vals[length:neb_size**2 + length] = np.reshape(tvals, (tvals.shape[0], 1))
+      length = length + neb_size**2
+
+  vals = vals(1:length)
+  row_inds=row_inds(1:length)
+  col_inds=col_inds(1:length)
   A=sparse(row_inds,col_inds,vals,img_size,img_size)
 
   sumA = sum(A,2)
