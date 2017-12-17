5d27346bffbcaef5713f832543832377ceaefa1d
rubychilds <ruby.childs1@gmail.com>
2017-12-17 22:11:59 +0000

    correction of reshape

--- gen_laplacian/reshape_img.py
+++ gen_laplacian/reshape_img.py
@@ -1,13 +1,16 @@
+import PIL.Image
+import math
+
+
 def reshape_img(im, length, nargin=2):
     if nargin < 2:
         length = 512
 
-    [h, w, ~] = im.shape
+    w, h = im.size
     if h > w:
         h2 = length
-        w2 = floor(w * h2 / h)
+        w2 = math.floor(w * h2 / h)
     else:
         w2 = length
-        h2 = floor(h * w2 / w);
-
-    return imresize(im, [h2 w2])
+        h2 = math.floor(h * w2 / w);
+    return im.resize((w2, h2))
